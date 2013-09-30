122b98332dd2df97c1551e42b76b14f373ee0afd
Aldous Latonio <aldouslatonio@gmail.com>
2013-09-30 16:02:38 +0800

    Initial commit 	-generated maven webapp archetype 	-configured pom.xml, configured web.xml 	-created servlet 	-initialized maven jetty plugin 	-tested servlet on jetty

---
+++ servlet/src/main/java/com/test/app/ServletTest.java
@@ -0,0 +1,40 @@
+package com.test.app;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletTest
+ */
+public class ServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().println("<h1>Servlet</h1>");
+		response.getWriter().println("session=" + request.getSession(true).getId());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
